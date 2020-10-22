ccf071ceda5a931a76caa13f7e8464b86ee1c722
EC2 Default User <ec2-user@ip-172-31-72-97.ec2.internal>
2020-10-22 02:13:36 +0000

    カラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:name, :introduction]
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:added_attrs])
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:added_attrs])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
