31be65f69f1f2ade009514b024fbbf674eb03515
EC2 Default User <ec2-user@ip-172-31-72-97.ec2.internal>
2020-10-22 03:56:55 +0000

    [Add]投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,16 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save(book_params)
+    redirect_to books_path
   end
+
+  def index
+    @book = Book.all
+  end
+
 
   def show
   end
@@ -13,10 +22,13 @@ class BooksController < ApplicationController
   end
 
   def update
+
   end
 
-  def destroy
-  end
 
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   attachment :profile_image
+
+  has_many :books, dependent: :destroy
 end
